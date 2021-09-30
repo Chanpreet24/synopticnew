3ef920a425d746ddb656d7705141610e14c4bf9f
Chanpreet24 <62950691+Chanpreet24@users.noreply.github.com>
2021-09-30 13:14:23 +0100

    proejct files added

---
+++ Synopticsrc/administratorGame.js
@@ -0,0 +1,14 @@
+
+
+document.querySelector('#log-out-button').addEventListener('click', () => {
+    window.location.href = './loginPage.html';
+});
+
+document.querySelector('#Edit-playerButton').addEventListener('click', () => {
+    window.location.href = './editPlayer.html';
+});
+
+document.querySelector('#Edit-quizButton').addEventListener('click', () => {
+    window.location.href = './editQuiz.html';
+});
+
