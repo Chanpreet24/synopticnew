061955594dd6e1eb45c46c016d3233dfb5e65d8e
Chanpreet24 <62950691+Chanpreet24@users.noreply.github.com>
2021-10-08 16:19:07 +0100

    new admin.js added

--- administratorGame.js
+++ administratorGame.js
@@ -10,3 +10,65 @@ document.querySelector('#Edit-quizButton').addEventListener('click', () => {
     window.location.href = './editQuiz.html';
 });
 
+
+// function linkCategorytoDB (){
+//     db.allDocs({
+//         include_docs: true
+//       }).then(function (result) {
+//        var quizName = result.rows.quizName;
+//        console.log(quizName)
+//     });
+// }
+
+function getdetails(e){
+    e.preventDefault();
+    var e = document.getElementById("quizFormControl");
+    var quizCategoryName = e.value;
+    //alert(quizCategoryName);
+db.allDocs({
+        include_docs: true
+      }).then(function (result) {
+       var quizName = result.rows;
+       console.log(quizName);
+       $("#quizTable tr").remove(); 
+       quizName.forEach(element => {
+        if(element.doc.quizName == quizCategoryName)
+           var row = "<tr><td>" + element.doc.quizName + "</td><td>" + element.doc.question + "</td><td>" + "<td>" + element.doc.correctanswer + "</td><td>" + "<button>Edit Question</button>" + "</td><td>" + "<button>Delete Question</button>" + "</td></tr>";
+           $("#quizTable").append(row);
+       });
+      }).catch(function (err) {
+        console.log(err);
+      });
+}
+
+$('#quizFormControl').on('change', getdetails);
+
+$('#test').on('click', linkCategorytoDB);
+
+
+//I need to dynamically add the quizzes into this array from the quiz db, based on the quiz name stored in the db
+
+var categories = [
+    'films','sports', 'numbers'
+];
+
+
+var option = "";
+
+for(var i =0; i< categories.length; i++)
+{
+    option += '<option value="' + categories[i] + '">' + categories[i] + "</option>"
+}
+document.getElementById('quizFormControl').innerHTML = option
+
+
+// var as = document.forms[0].ddlViewBy.value;
+//   var strUser = e.options[e.selectedIndex].value;
+//   console.log(as, strUser);
+
+
+
+
+
+
+//On the click of the option, the questions and answers need to be displayed in a table
